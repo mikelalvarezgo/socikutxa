563d5de1f4758f2cb550e67679787f3603d018e1
mikelalvarezgo <mikelalvarezgo@gmail.com>
2025-11-05 16:04:24 +0100

    Complete import products from xlx functionality

--- src/main/scala/com/mikelalvarezgo/socikutxa/product/infrastructure/ProductController.scala
+++ src/main/scala/com/mikelalvarezgo/socikutxa/product/infrastructure/ProductController.scala
@@ -2,9 +2,9 @@ package com.mikelalvarezgo.socikutxa.product.infrastructure
 
 import cats.effect.IO
 import com.mikelalvarezgo.socikutxa.product.application.GetProductsUseCase.GetProductsQuery
+import com.mikelalvarezgo.socikutxa.product.application.ImportProductsUseCase.ImportProductsCommand
 import com.mikelalvarezgo.socikutxa.product.application.{GetProductsUseCase, ImportProductsUseCase}
 import com.mikelalvarezgo.socikutxa.product.domain.Product
-import com.mikelalvarezgo.socikutxa.product.infrastructure.ProductJsonCodec._
 import com.mikelalvarezgo.socikutxa.shared.infrastructure.http.HttpErrorHandler._
 import io.circe.Encoder
 import io.circe.generic.auto._
@@ -21,9 +21,23 @@ class ProductController(
 
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case req @ POST -> Root / "product" / "upload" =>
-      req.decode[Multipart[IO]] {
-        _ =>
-          Ok("Upload received")
+      req.decode[Multipart[IO]] { multipart =>
+        multipart.parts.find(_.filename.exists(_.endsWith(".xlsx"))) match {
+          case Some(filePart) =>
+            filePart.body.compile.toList.flatMap { chunks =>
+              val bytes       = chunks.toArray
+              val inputStream = new java.io.ByteArrayInputStream(bytes)
+              XlsxProductParser.parse(inputStream) match {
+                case Right(products) =>
+                  val command = ImportProductsCommand(products)
+                  importProductsUseCase.execute(command).toHttpResponse
+                case Left(error)     =>
+                  BadRequest(error.getMessage)
+              }
+            }
+          case None           =>
+            BadRequest("No .xlsx file found in upload")
+        }
       }
     case GET -> Root / "product"                   =>
       getProductsUseCase.execute(GetProductsQuery()).toHttpResponse
